707dc934e127c9507d591e08e0d96d76d04cdbdd
Felipe Pacheco Schürmann <felipe.xirma@gmail.com>
2020-11-10 12:02:38 +0000

    rooms component and button component done

---
+++ src/app/modules/rooms/components/buttom/buttom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buttom',
+  templateUrl: './buttom.component.html',
+  styleUrls: ['./buttom.component.scss']
+})
+export class ButtomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
