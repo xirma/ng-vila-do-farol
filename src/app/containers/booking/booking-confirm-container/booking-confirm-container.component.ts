10a88062fe3088f76e5255a0d11a28d03c7c8aa3
Felipe Pacheco Schürmann <felipe.xirma@gmail.com>
2020-11-24 17:44:22 +0000

    booking confirm container created

---
+++ src/app/containers/booking/booking-confirm-container/booking-confirm-container.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-confirm-container',
+  template: `
+    <p>
+      booking-confirm-container works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class BookingConfirmContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
